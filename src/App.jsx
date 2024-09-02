46d5aa9e1f0fae7b4bc2cde00d4a129f3e064e57
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-09-02 15:35:48 +0600

    Count functionality added with Redux

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Counter from './features/counter/Counter.jsx'
+
+function App() {
+
+  return (
+    <main>
+      <h3>Increment your count</h3>
+      <Counter />
+    </main>
+  )
+}
+
+export default App
